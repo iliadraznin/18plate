d4b4c9c764b68c1877a87ec2ecc00b4dd7ab1717
ilia <ilia.draznin@gmail.com>
2013-05-01 23:50:22 -0400

    Added console  fix
    Added the function to avoid console errors if they're not supported.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+// Avoid `console` errors in browsers that lack a console
+// Based on HTML5 Boilerplate https://github.com/h5bp/html5-boilerplate/blob/master/js/plugins.js
+(function() {
+	var method,
+		noop = function() {},
+		methods = [
+			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+			'timeStamp', 'trace', 'warn'
+		],
+		length = methods.length,
+		console = (window.console = window.console || {});
+
+	while (length--) {
+	    method = methods[length];
+	    console[method] = console[method] || noop;
+	}
+}());
+
 $(function(){
 	// your code here
 });
